8f998260f6021bffbc6337c812406f13e230453c
Omid Astaraki <omid.ocean@gmail.com>
2019-05-08 23:44:26 +0430

    now using generated backend

---
+++ src/generators/rGenerator.js
@@ -0,0 +1,18 @@
+export const rScript = pathObj => {
+  return `require(jsonlite)\n\\nnodes <- read.csv("${pathObj.populationPath.replace(
+    /\\/g,
+    '/'
+  )}", header=T, as.is=T)\nlinks <- read.csv("${pathObj.networkPath.replace(
+    /\\/g,
+    '/'
+  )}", header=T, as.is=T)\nlink=as.matrix(links)\nnode=as.matrix(nodes)\n#city=readline(prompt = "Enter city name : ")\n\nSIR <- function(city,I0, beta, alpha, t) {\n  a=which(node==city,arr.ind = T)\n  N=as.numeric(node[[a[1],2]])\n  S <- numeric(t)\n  I <- numeric(t)\n  S[1] <- N-I0\n  I[1] <- I0\n  IT=0\n\n   #p adad tasadofi\n  aa=which(link[,2]==city,arr.ind = T)\n\n\n\n    IT=sum(as.numeric(link[aa,3]))\n\n  IT\n\n  IIT=IT*0.001\n\n  IIT\n  IIT=round(IIT)\n  IIT\n\n  ab=which(link[,1]==city,arr.ind = T)\n  IF=sum(as.numeric(link[ab,3]))\n\n  IIF=round(IF*0.001)\n  IIF\n matIIF <- matrix(IIF, nrow=1, ncol=t)\n\n matIIT<- matrix(IIT,nrow = 1,ncol = t)\n\n\n  ST=(IT-IIT)\n\n  SF=(IF-IIF)\n  matST<-matrix(ST,nrow=1,ncol = t)\n  matSF<-matrix(SF,nrow=1,ncol=t)\n\n  ST\n  SF\n\n  for (i in 2:t) {\n     {\n\n    }\n    S[i] <- S[i-1] - beta*S[i-1]/N*I[i-1]+matST[i-1]-matSF[i-1]\n    I[i] <- I[i-1] + beta*S[i-1]/N*I[i-1] - alpha * I[i-1]+matIIT[i-1]-matIIF[i-1]\n    if (I[i] < 1 || S[i] < 1)\n      break\n  }\n  df <- data.frame(time=1:t, S=S, I=I, R=N-S-I)\n  #df <- df[S>1&I>1,]\n\n  #df$AR <- (df$I+df$R)/N\n  #nr <- nrow(df)\n  #rate <- df$I[2:nr]/df$I[1:(nr-1)]\n  #df$rate <- c(rate[1], rate)\n  return(df)\n}\n\n\n#read from json file\nresult <- fromJSON("${pathObj.inputPath.replace(
+    /\\/g,
+    '/'
+  )}")\n\njson_data_frame <- as.data.frame(result)\nialpha=as.numeric(as.character(json_data_frame$healing_rate))\nibeta=as.numeric(as.character(json_data_frame$infection_rate))\niIO=as.numeric(as.character(json_data_frame$infected_population))\nicity=as.character(json_data_frame$city_name)\nit=as.numeric(as.character(json_data_frame$sim_time))\ndf <- SIR(icity,iIO,ibeta,ialpha,it)\n\n#make json file\n\nwrite_json(df, "${pathObj.outSIRPath.replace(
+    /\\/g,
+    '/'
+  )}")\n\n###############Number of hospital need\n\ndi=data.frame(df$I)\nbed=df$I\nbed=round((0.003*df$I))\nbed=as.data.frame(bed)\ndb=merge(di,bed)\nwrite_json(db, "${pathObj.outBedPath.replace(
+    /\\/g,
+    '/'
+  )}")`;
+};
--- src/helpers/fileLoader.js
+++ src/helpers/fileLoader.js
@@ -1,14 +1,19 @@
 import fs from 'fs';
 import path from 'path';
 import { dialog } from 'electron';
+import { homedir } from 'os';
+import {
+  generateNetwork,
+  generatePopulation
+} from '../generators/csvGenerator';
+import { rScript } from '../generators/rGenerator';
 
 class FileLoader {
   constructor() {
-    this.saveDir = 'D:\\health\\';
+    this.saveDir = path.join(homedir(), 'healths');
     this.inputDir = path.join(this.saveDir, 'inputDir');
     this.outputDir = path.join(this.saveDir, 'outputDir');
     this.rExec = path.join(this.saveDir, 'SIR_final.R');
-
     if (!fs.existsSync(this.saveDir)) {
       fs.mkdirSync(this.saveDir);
     }
@@ -18,34 +23,41 @@ class FileLoader {
     if (!fs.existsSync(this.outputDir)) {
       fs.mkdirSync(this.outputDir);
     }
+
+    this.pathObj = {
+      populationPath: path.join(this.inputDir, 'population.csv'),
+      networkPath: path.join(this.inputDir, 'networkcsv.csv'),
+      outBedPath: path.join(this.outputDir, 'outbed.json'),
+      outSIRPath: path.join(this.outputDir, 'outsir.json'),
+      inputPath: path.join(this.inputDir, 'input.json')
+    };
+
+    this.writeBackEndFiles();
   }
 
   saveInputAsJson(inputData) {
-    fs.writeFile(
-      path.join(this.inputDir, 'input.json'),
-      inputData,
-      'utf8',
-      e => {
-        if (e) {
-          dialog.showErrorBox(
-            'خطا در ذخیره ورودی',
-            'خطایی در ذخیره ورودی در سیستم به وجود آمد. لطفا دوباره تلاش کنید!'
-          );
-        }
+    fs.writeFile(this.pathObj.inputPath, inputData, 'utf8', e => {
+      if (e) {
+        dialog.showErrorBox(
+          'خطا در ذخیره ورودی',
+          'خطایی در ذخیره ورودی در سیستم به وجود آمد. لطفا دوباره تلاش کنید!'
+        );
       }
-    );
+    });
   }
 
   getChartOutputObj() {
-    return JSON.parse(
-      fs.readFileSync(path.join(this.outputDir, 'outsir.json'))
-    );
+    return JSON.parse(fs.readFileSync(this.pathObj.outSIRPath));
   }
 
   getTableOutputObj() {
-    return JSON.parse(
-      fs.readFileSync(path.join(this.outputDir, 'outbed.json'))
-    );
+    return JSON.parse(fs.readFileSync(this.pathObj.outBedPath));
+  }
+
+  writeBackEndFiles() {
+    fs.writeFile(this.pathObj.networkPath, generateNetwork());
+    fs.writeFile(this.pathObj.populationPath, generatePopulation());
+    fs.writeFile(this.rExec, rScript(this.pathObj));
   }
 }
 const instance = new FileLoader();
